414daa27b5aa879e00cb034e8b60c02b2dfcf1b4
Michael Snoyman <michael@snoyman.com>
2012-04-09 07:47:51 +0300

    Fix upload script

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ cabal build
 strip dist/build/ipredictthat/ipredictthat
 bzip2 dist/build/ipredictthat/ipredictthat
 scp -r dist/build/ipredictthat/ipredictthat.bz2 ubuntu@www.yesodweb.com:/home/ubuntu/ipredictthat
-ssh ubuntu@www.yesodweb.com 'cd ipredictthat.com && git pull && mv ipredictthat ipredictthat.old && bunzip2 ipredictthat.bz2 && sudo restart whosawthatcoming'
+ssh ubuntu@www.yesodweb.com 'cd ipredictthat && git pull && mv ipredictthat ipredictthat.old && bunzip2 ipredictthat.bz2 && sudo restart whosawthatcoming'
