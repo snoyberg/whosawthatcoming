61229ab0ec6246ca077d7eee883fb4e54cff1739
Michael Snoyman <michael@snoyman.com>
2012-04-09 07:22:33 +0300

    Added upload

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+cabal clean
+cabal configure
+cabal build
+strip dist/build/ipredictthat/ipredictthat
+bzip2 dist/build/ipredictthat/ipredictthat
+scp -r dist/build/ipredictthat/ipredictthat.bz2 ubuntu@www.yesodweb.com:/home/ubuntu/ipredictthat
+ssh ubuntu@www.yesodweb.com 'cd ipredictthat.com && git pull && mv ipredictthat ipredictthat.old && bunzip2 ipredictthat.bz2 && sudo restart whosawthatcoming'
