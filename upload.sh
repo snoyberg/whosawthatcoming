2d1f6f7f3b9100b050998d24aa03dc1c43918f88
Michael Snoyman <michael@snoyman.com>
2012-04-10 19:06:37 +0300

    Updated upload

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ cabal build
 strip dist/build/whosawthatcoming/whosawthatcoming
 bzip2 dist/build/whosawthatcoming/whosawthatcoming
 scp -r dist/build/whosawthatcoming/whosawthatcoming.bz2 ubuntu@www.yesodweb.com:/home/ubuntu/whosawthatcoming
-ssh ubuntu@www.yesodweb.com 'cd whosawthatcoming && git pull && mv whosawthatcoming whosawthatcoming.old && bunzip2 whosawthatcoming.bz2 && sudo restart whosawthatcoming'
+ssh ubuntu@www.yesodweb.com 'cd whosawthatcoming && git fetch && git merge origin/master && mv whosawthatcoming whosawthatcoming.old && bunzip2 whosawthatcoming.bz2 && sudo restart whosawthatcoming'
