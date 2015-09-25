e42f8679e9199ceb1cd4dcd21c848f989f3faf23
Michael Snoyman <michael@snoyman.com>
2015-09-25 11:39:58 +0300

    Kubernetes

--- Model.hs
+++ Model.hs
@@ -4,14 +4,21 @@ module Model where
 
 import Prelude
 import Yesod
-import Data.Text (Text)
+import Data.Text (Text, append)
 import Database.Persist.Quasi
 import Data.Time (UTCTime)
 import Data.Typeable (Typeable)
+import qualified Data.Text as T
+import Data.Char (isUpper)
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
-    $(persistFileWith lowerCaseSettings "config/models")
+    $(persistFileWith upperCaseSettings
+        { psToDBName = \t ->
+            if not (T.null t) && isUpper (T.head t)
+                then "WhoSawThatComing__" `append` psToDBName upperCaseSettings t
+                else psToDBName upperCaseSettings t
+        } "config/models")
